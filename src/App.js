32cc67ebf5129be0f60f8fa0cf6d544f1c8b4fe1
AFresnedo <AFresnedo@users.noreply.github.com>
2018-09-12 09:18:25 -0700

    fix no-prop for landing bug

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Landing user='userId' />
+        <Landing user='userId' sections={[]}/>
       </div>
     );
   }
